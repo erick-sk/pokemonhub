711d79a39190bf2ed0cb690d68875fafb1602f77
Erick Saavedra <esvdrah@gmail.com>
2022-03-10 16:27:55 -0500

    Add normalize.css and custom config of global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 function App() {
-  return <div>DEUNA-Challenge</div>;
+  return (
+    <>
+      <h1>DEUNA-Challenge</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt
+        inventore mollitia voluptates laboriosam quia harum est cumque iste
+        ducimus, hic corrupti voluptas consequuntur aut totam repellendus quidem
+        fuga id pariatur.
+      </p>
+    </>
+  );
 }
 
 export default App;
